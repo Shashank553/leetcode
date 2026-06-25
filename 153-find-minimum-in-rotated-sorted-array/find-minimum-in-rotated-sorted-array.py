60610f53d65e558c70eecd9b5da369da8d7a1300
Gujapaneni shashank sreenivas <2300033058@kluniversity.in>
2026-06-25 10:29:46 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (33.72%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -4,6 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nums.sort()
-        return nums[0]
+        '''nums.sort()
+        return nums[0]'''
+        #by using bunary search solution
+        left=0
+        right=len(nums)-1
+        while left<right:
+            mid=(left+right)//2
+
+            if nums[mid]>nums[right]:
+                #minimum is in the right half
+                left=mid+1
+
+            else:
+                #minimum is at  mid or in the left half
+                right=mid
+        return nums[left]
         
