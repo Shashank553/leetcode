91b38dddc80c488e92154db04db7b0e38a7aeef5
G. Shashank sreenivas <2300033058@kluniversity.in>
2026-06-29 14:32:18 +0530

    Time: 5 ms (21.57%) | Memory: 12.6 MB (11.66%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack=[]
+        pairs={')':'(',']':'[','}':'{'}
+        for ch in s:
+            if ch in pairs.values():
+                stack.append(ch)
+            else:
+                if not stack or stack.pop()!=pairs[ch]:
+                    return False
+        return not stack
+        
