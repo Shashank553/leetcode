af1ae5cf647cd7ccd6cb96e2b00cc9c377224c65
G. Shashank sreenivas <2300033058@kluniversity.in>
2026-07-23 21:49:08 +0530

    Time: 0 ms (100.00%) | Memory: 21.3 MB (62.50%) - LeetSync

---
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def uniqueXorTriplets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n=len(nums)
+        if n==1:
+            return 1
+        if n==2:
+            return 2
+
+        return 1<< n.bit_length()
