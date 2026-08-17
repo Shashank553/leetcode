af483ae52a95858ec9f45acd07702eb5c8799259
G. Shashank sreenivas <2300033058@kluniversity.in>
2026-08-17 19:43:15 +0530

    Time: 126 ms (30.34%) | Memory: 19.5 MB (13.52%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,4 +1,4 @@
-class Solution:
+'''class Solution:
     def maxProfit(self, prices):
         low = prices[0]
         profit = 0
@@ -9,4 +9,19 @@ class Solution:
             else:
                 profit = max(profit, prices[i] - low)
 
-        return profit
+        return profit'''
+#kadane algorithm
+class Solution:
+    def maxProfit(self, prices):
+        current = 0
+        maximum = 0
+
+        for i in range(1, len(prices)):
+            current += prices[i] - prices[i - 1]
+
+            if current < 0:
+                current = 0
+
+            maximum = max(maximum, current)
+
+        return maximum
