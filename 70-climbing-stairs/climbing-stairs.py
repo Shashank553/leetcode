88dcbe573eedd0c91ac6d046413f92b6960489c8
G. Shashank sreenivas <2300033058@kluniversity.in>
2026-08-30 10:47:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (57.51%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,10 +1,14 @@
-#this follows the fibonacci series
+#Dynamic programing
 
 class Solution(object):
     def climbStairs(self, n):
         if n==1:
             return 1
-        a,b=1,2
+        dp=[0]*(n+1) 
+        dp[1]=1
+        dp[2]=2
+
         for i in range(3,n+1):
-            a,b=b,a+b
-        return b
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
+         
